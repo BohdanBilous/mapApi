5361715ef57faf6324fc2cd7bec4298fa4393489
Bohdan <bohdan.bilous2@gmail.com>
2019-09-15 18:20:22 +0300

    page resiurces final

--- js/modules/resources.js
+++ js/modules/resources.js
@@ -4,6 +4,7 @@
 import { html, desktop, mobile, exists, scrollFromScreen, filterSidebarInit } from '../generic-helpers';
 import { PageNav } from '../page-nav-anchor';
 import { ParallaxScroll } from '../parallax-scroll';
+import { customSelect } from '../custom-select';
 
 window.addEventListener("load", function() {
 
@@ -13,11 +14,44 @@ window.addEventListener("load", function() {
     }
 
     // Scroll From Screen
-    if (exists(".hint-from-top")) scrollFromScreen(".title-top");
+    if (exists(".hint-from-top")) scrollFromScreen(".screen-top");
 
     // Parallax Scroll
     if (exists(".screen-head")) {
         new ParallaxScroll(".screen-head", ".screen-bg", 200);
+    }
+
+    // Select Custom
+    if (exists(".select-custom")) {
+        customSelect();
+        const selectIndustriesIcon = document.querySelector(".select-industries-icon img");
+        const selectIndustries =  document.querySelector(".select-custom select");
+        
+        selectIndustries.addEventListener("change", () => {
+            let index = selectIndustries.value;
+            selectIndustriesIcon.src = `images/ind-${index}.svg`;
+            
+            loadContent(index);
+            changeFileList(index);
+        });
+
+        let loadContent = (index) => {
+            let xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function() {
+                if (this.readyState == 4 && this.status == 200) {
+                    document.getElementById("content-load").innerHTML = this.responseText;
+                }
+            };
+            xhttp.open("GET", `_res-content-${index}.html`, true);
+            xhttp.send();
+        }
+
+        let changeFileList = (index) => {
+            if (exists(".file-list")) {
+                document.querySelector(".file-list.show").classList.remove("show");
+                document.querySelector(`#file-list-${index}`).classList.add("show");
+            }
+        }
     }
 
     // Filter Mobile
