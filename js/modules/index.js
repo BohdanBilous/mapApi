1363741aebe68749434fcffa2dd923814a8bb102
Bohdan <bohdan.bilous2@gmail.com>
2019-09-16 18:15:16 +0300

    feedback parallax add

--- js/modules/index.js
+++ js/modules/index.js
@@ -18,6 +18,27 @@ window.addEventListener("load", function() {
     // Mission Fader
     let missinFader = new SuperSlider(".mission-fader", "fader");
 
+    const parallaxBox = document.querySelector(".feedback-fader");
+    let imgOffsetLeft = document.querySelector(".feedback-img").offsetLeft * 0.01;
+    let imgList = document.querySelectorAll(".feedback-img img");
+    let x;
+
+    imgList.forEach( (img) => {
+        parallaxBox.addEventListener("mousemove", (event) => {
+            event = event || window.event;
+            x = event.clientX - parallaxBox.offsetLeft;
+
+            mouseParallax(parallaxBox, img, imgOffsetLeft, x, 30);
+        });
+    });
+
+    function mouseParallax(container, obj, left, mouseX, speed) {
+        let containerWidth = parseInt(container.clientWidth);
+        let moveValue = left - ( ( ( mouseX - ( parseInt( obj.offsetWidth ) / 1 + left ) ) / containerWidth ) * speed );
+        console.log();
+        obj.style = "transform: translateX(" + moveValue + "px)";
+    }
+
     // Feedback Fader
     let feedbackFader = new SuperSlider(".feedback-fader", "fader");
     if (desktop) new CursorFader(".feedback-fader", sectionScroll.sectionOneHeight);
