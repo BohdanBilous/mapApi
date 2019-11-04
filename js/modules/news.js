d1bb612f5097a3ce59a7d96066f4df39094bc82c
Bohdan <bohdan.bilous2@gmail.com>
2019-11-04 11:41:12 +0200

    news js fix

--- js/modules/news.js
+++ js/modules/news.js
@@ -1,75 +1,85 @@
 /*jshint esversion: 6 */
 
 /* MODULE IMPORTS */
-import { html, desktop, mobile, exists, scrollFromScreen, tags, filterSidebarInit } from '../generic-helpers';
-import { PageNav } from '../page-nav-anchor';
-import { ParallaxScroll } from '../parallax-scroll';
-import { ImageCursor } from '../image-cursor';
-import { MediaLoader } from '../media-loader';
+import {
+  html,
+  desktop,
+  mobile,
+  exists,
+  scrollFromScreen,
+  tags,
+  filterSidebarInit
+} from "../generic-helpers";
+import { PageNav } from "../page-nav-anchor";
+import { ParallaxScroll } from "../parallax-scroll";
+import { ImageCursor } from "../image-cursor";
+import { MediaLoader } from "../media-loader";
 
 window.addEventListener("load", function() {
+  // Page Nav
+  if (exists(".media-stream--container")) {
+    let pageNav = new PageNav(".media-stream--container", ".checkpoint");
+  }
 
-    // Page Nav
-    if (exists(".media-stream--container")) {
-        let pageNav = new PageNav(".media-stream--container", ".checkpoint");
-    }
+  // Scroll From Screen
+  if (exists(".hint-from-top")) scrollFromScreen(".screen-top");
 
-    // Scroll From Screen
-    if (exists(".hint-from-top")) scrollFromScreen(".screen-top");
+  // Tags
+  if (exists(".tags-more")) tags();
 
-    // Tags
-    if (exists(".tags-more")) tags();
+  // Filter Mobile
+  filterSidebarInit();
 
-    // Filter Mobile
-    filterSidebarInit();
+  // Parallax Scroll
+  if (exists(".screen-head")) {
+    new ParallaxScroll(".screen-head", ".screen-bg", 200);
+  }
 
-    // Parallax Scroll
-    if (exists(".screen-head")) {
-        new ParallaxScroll(".screen-head", ".screen-bg", 200);
-    }
+  // Lazy Loader Images
+  if (exists(".lazy-img")) {
+    const images = document.querySelectorAll(".lazy-img");
 
-    // Lazy Loader Images
-    if (exists(".lazy-img")) {
-        const images = document.querySelectorAll(".lazy-img");
+    images.forEach(image => {
+      const mediaQueryImages = [
+        { media: "screen and (min-width: 768px)", src: image.dataset.bg },
+        { media: "screen and (max-width: 767px)", src: image.dataset.bgmob }
+      ];
 
-        images.forEach( image => {
-            const mediaQueryImages = [
-                { media: "screen and (min-width: 768px)", src: image.dataset.bg }, 
-                { media: "screen and (max-width: 767px)", src: image.dataset.bgmob }
-            ];
-    
-            new MediaLoader(image, "image", mediaQueryImages);
-        });
-    }
-    
-    if (exists(".lazy-bg")) {
-        const bgs = document.querySelectorAll(".lazy-bg");
+      new MediaLoader(image, "image", mediaQueryImages);
+    });
+  }
 
-        bgs.forEach( bg => {
-            const mediaQueryBackgrounds = [
-                { media: "screen and (min-width: 768px)", src: bg.dataset.bg }, 
-                { media: "screen and (max-width: 767px)", src: bg.dataset.bgmob }
-            ];
-    
-            new MediaLoader(bg, "background", mediaQueryBackgrounds);
-        });
-    }
+  if (exists(".lazy-bg")) {
+    const bgs = document.querySelectorAll(".lazy-bg");
 
-    // Events Cursor Image
-    if (desktop) {
-        const eventsTitles = document.querySelectorAll(".events-table-row .events-ttl-section");
+    bgs.forEach(bg => {
+      const mediaQueryBackgrounds = [
+        { media: "screen and (min-width: 768px)", src: bg.dataset.bg },
+        { media: "screen and (max-width: 767px)", src: bg.dataset.bgmob }
+      ];
 
-        eventsTitles.forEach( eventsTitle => {
-            let eventsWrapOffsetTop  = document.querySelector(".events-table").offsetTop;
-            let eventsWrapOffsetLeft = document.querySelector(".events-table-cell-m").offsetLeft;
+      new MediaLoader(bg, "background", mediaQueryBackgrounds);
+    });
+  }
 
-            new ImageCursor(
-                eventsTitle, 
-                ".events-img", 
-                eventsWrapOffsetLeft, 
-                (eventsWrapOffsetTop + window.innerHeight)
-            );
-        });
-    }
-        
+  // Events Cursor Image
+  if (desktop) {
+    const eventsTitles = document.querySelectorAll(
+      ".events-table-row .events-ttl-section"
+    );
+
+    eventsTitles.forEach(eventsTitle => {
+      let eventsWrapOffsetTop = document.querySelector(".events-table")
+        .offsetTop;
+      let eventsWrapOffsetLeft = document.querySelector(".events-table-cell-m")
+        .offsetLeft;
+
+      new ImageCursor(
+        eventsTitle,
+        ".events-img",
+        eventsWrapOffsetLeft,
+        eventsWrapOffsetTop + window.innerHeight
+      );
+    });
+  }
 });
