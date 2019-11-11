fe3fd4889d387e27355ddbcb5c5f84437ed43e0b
Bohdan <bohdan.bilous2@gmail.com>
2019-11-11 21:19:16 +0200

    some fixes

--- js/modules/index.js
+++ js/modules/index.js
@@ -1,71 +1,88 @@
 /*jshint esversion: 6 */
 
 /* MODULE IMPORTS */
-import { html, desktop, mobile, exists, scrollFromScreen } from '../generic-helpers';
-import { SolutionsInd } from '../solutions-ind';
-import { SuperSlider } from '../super-slider';
-import { CursorFader } from '../fader-cursor';
-import { SectionScroll } from '../section-scroll';
-import { ImageCursor } from '../image-cursor';
+import {
+  html,
+  desktop,
+  mobile,
+  exists,
+  scrollFromScreen
+} from "../generic-helpers";
+import { SolutionsInd } from "../solutions-ind";
+import { SuperSlider } from "../super-slider";
+import { CursorFader } from "../fader-cursor";
+import { SectionScroll } from "../section-scroll";
+import { ImageCursor } from "../image-cursor";
 
 window.addEventListener("load", function() {
+  // Solutions
+  let solutionInd = new SolutionsInd();
 
-    // Solutions
-    let solutionInd = new SolutionsInd();
+  // Scrolling
+  let sectionScroll = new SectionScroll();
 
-    // Scrolling
-    let sectionScroll = new SectionScroll();
-    
-    // Mission Fader
-    let missinFader = new SuperSlider(".mission-fader", "fader");
+  // Mission Fader
+  new SuperSlider(".mission-fader", "fader");
 
+  // Feedback Fader
+  if (exists(".feedback-fader")) {
     const parallaxBox = document.querySelector(".feedback-fader");
-    let imgOffsetLeft = document.querySelector(".feedback-img").offsetLeft * 0.01;
+    let imgOffsetLeft =
+      document.querySelector(".feedback-img").offsetLeft * 0.01;
     let imgList = document.querySelectorAll(".feedback-img img");
     let x;
 
-    imgList.forEach( (img) => {
-        parallaxBox.addEventListener("mousemove", event => {
-            event = event || window.event;
-            x = event.clientX - parallaxBox.offsetLeft;
+    imgList.forEach(img => {
+      parallaxBox.addEventListener("mousemove", event => {
+        event = event || window.event;
+        x = event.clientX - parallaxBox.offsetLeft;
 
-            mouseParallax(parallaxBox, img, imgOffsetLeft, x, 30);
-        });
+        mouseParallax(parallaxBox, img, imgOffsetLeft, x, 30);
+      });
     });
 
     function mouseParallax(container, obj, left, mouseX, speed) {
-        let containerWidth = parseInt(container.clientWidth);
-        let moveValue = left - ( ( ( mouseX - ( parseInt( obj.offsetWidth ) / 1 + left ) ) / containerWidth ) * speed );
+      let containerWidth = parseInt(container.clientWidth);
+      let moveValue =
+        left -
+        ((mouseX - (parseInt(obj.offsetWidth) / 1 + left)) / containerWidth) *
+          speed;
 
-        obj.style = "transform: translateX(" + moveValue + "px)";
-    }    
-
-    // Feedback Fader
-    let feedbackFader = new SuperSlider(".feedback-fader", "fader");
-    if (desktop) new CursorFader(".feedback-fader", sectionScroll.sectionOneHeight);
-
-    // Graph
-    if (mobile && exists(".graph-scroll")) {
-        document.querySelector(".graph-scroll").scrollLeft = (window.innerWidth / 4);
+      obj.style = "transform: translateX(" + moveValue + "px)";
     }
 
-    // Events Cursor Image
-    if (desktop) {
-        const eventsTitles = document.querySelectorAll(".events-table-row .events-ttl-section");
+    new SuperSlider(".feedback-fader", "fader");
+    if (desktop)
+      new CursorFader(".feedback-fader", sectionScroll.sectionOneHeight);
+  }
 
-        eventsTitles.forEach( eventsTitle => {
-            let eventsWrapOffsetTop  = document.querySelector(".events-wrap").offsetTop;
-            let eventsWrapOffsetLeft = document.querySelector(".events-wrap .layout-col-r").offsetLeft;
+  // Graph
+  if (mobile && exists(".graph-scroll")) {
+    document.querySelector(".graph-scroll").scrollLeft = window.innerWidth / 4;
+  }
 
-            new ImageCursor(
-                eventsTitle, 
-                ".events-img", 
-                eventsWrapOffsetLeft,
-                (eventsWrapOffsetTop + sectionScroll.sectionOneHeight)
-            );
-        });
-    }
-    
-    // Scroll From Screen
-    if (exists(".hint-from-top")) scrollFromScreen(".screen-top");
+  // Events Cursor Image
+  if (desktop) {
+    const eventsTitles = document.querySelectorAll(
+      ".events-table-row .events-ttl-section"
+    );
+
+    eventsTitles.forEach(eventsTitle => {
+      let eventsWrapOffsetTop = document.querySelector(".events-wrap")
+        .offsetTop;
+      let eventsWrapOffsetLeft = document.querySelector(
+        ".events-wrap .layout-col-r"
+      ).offsetLeft;
+
+      new ImageCursor(
+        eventsTitle,
+        ".events-img",
+        eventsWrapOffsetLeft,
+        eventsWrapOffsetTop + sectionScroll.sectionOneHeight
+      );
+    });
+  }
+
+  // Scroll From Screen
+  if (exists(".hint-from-top")) scrollFromScreen(".screen-top");
 });
