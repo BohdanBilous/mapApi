dd817c86159de602be14f2513e92f0f4cb99e719
new name <bar@example.com>
2019-11-12 16:07:14 +0200

    Create review page

---
+++ js/modules/reviews.js
@@ -0,0 +1,29 @@
+/*jshint esversion: 6 */
+
+/* MODULE IMPORTS */
+import {
+    html,
+    desktop,
+    mobile,
+    exists
+} from '../generic-helpers';
+
+window.addEventListener("load", function () {
+
+    const reviewsOpen = document.querySelectorAll(".reviews-opener");
+    reviewsOpen.forEach(item => {
+        let closeItm = item.parentNode.querySelector('.reviews-wrap')
+        closeItm.style.maxHeight = closeItm.scrollHeight + 41 + "px";
+        item.addEventListener("click", () => {
+            let childScroll = closeItm;
+            if (item.classList.contains("active")) {
+                item.classList.remove("active");
+                childScroll.style.maxHeight = childScroll.scrollHeight + 41 + "px";
+            } else {
+                item.classList.add("active");
+                childScroll.style.maxHeight = 0;
+            }
+        });
+    });
+
+});
