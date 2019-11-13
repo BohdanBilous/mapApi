5c9aa0c8595bd476b6812e32411440231c8d89ea
Bohdan <bohdan.bilous2@gmail.com>
2019-11-13 13:00:50 +0200

    rename iamge

---
+++ js/content-switcher.js
@@ -0,0 +1,13 @@
+/*
+    <div class="btn-switch" data-switch="word">
+
+    <span data-switch-content="word">content</span>
+*/
+
+export class ContentSwitcher {
+  constructor() {
+    this.clickHandler();
+  }
+
+  clickHandler() {}
+}
