c437715df7f20da39aeb6f9c026e613c1bc834c1
Bohdan <bohdan.bilous2@gmail.com>
2019-11-15 10:08:19 +0200

    footer social target blank

--- js/modules/index.js
+++ js/modules/index.js
@@ -8,36 +8,20 @@ import {
   exists,
   scrollFromScreen
 } from "../generic-helpers";
-import {
-  SolutionsInd
-} from "../solutions-ind";
-import {
-  SuperSlider
-} from "../super-slider";
-import {
-  CursorFader
-} from "../fader-cursor";
-import {
-  SectionScroll
-} from "../section-scroll";
-import {
-  ImageCursor
-} from "../image-cursor";
-import {
-  VideoLoad
-} from "../video-lazy-load";
+import { SolutionsInd } from "../solutions-ind";
+import { SuperSlider } from "../super-slider";
+import { CursorFader } from "../fader-cursor";
+import { SectionScroll } from "../section-scroll";
+import { ImageCursor } from "../image-cursor";
+import { VideoLoad } from "../video-lazy-load";
 
-
-
-window.addEventListener("load", function () {
-
-
+window.addEventListener("load", function() {
   // Solutions
   let solutionInd = new SolutionsInd();
 
   // Scrolling
   let sectionScroll = new SectionScroll();
- 
+
   // Mission Fader
   new SuperSlider(".mission-fader", "fader");
 
@@ -63,7 +47,7 @@ window.addEventListener("load", function () {
       let moveValue =
         left -
         ((mouseX - (parseInt(obj.offsetWidth) / 1 + left)) / containerWidth) *
-        speed;
+          speed;
 
       obj.style = "transform: translateX(" + moveValue + "px)";
     }
