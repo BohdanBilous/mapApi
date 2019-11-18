c7f630904e45c92d456ca97e620ed823c0caeb7c
Bohdan <bohdan.bilous2@gmail.com>
2019-11-18 17:12:16 +0200

    cooler svg change

--- js/modules/resources.js
+++ js/modules/resources.js
@@ -2,101 +2,90 @@
 
 /* MODULE IMPORTS */
 import {
-    html,
-    desktop,
-    mobile,
-    exists,
-    loadContent,
-    findParent,
-    scrollFromScreen,
-    imageLazy,
-    filterSidebarInit
-} from '../generic-helpers';
-import {
-    PageNav
-} from '../page-nav-anchor';
-import {
-    ParallaxScroll
-} from '../parallax-scroll';
-import {
-    customSelect
-} from '../custom-select';
-import {
-    MediaLoader
-} from '../media-loader';
+  exists,
+  loadContent,
+  findParent,
+  scrollFromScreen,
+  filterSidebarInit
+} from "../generic-helpers";
+import { PageNav } from "../page-nav-anchor";
+import { ParallaxScroll } from "../parallax-scroll";
+import { customSelect } from "../custom-select";
+import { MediaLoader } from "../media-loader";
 
-window.addEventListener("load", function () {
+window.addEventListener("load", function() {
+  // Page Nav
+  if (exists(".media-stream--container") && exists(".media-stream--section")) {
+    new PageNav(".media-stream--container", ".media-stream--section");
+  }
 
-    // Page Nav
-    if (exists(".media-stream--container") && exists(".media-stream--section")) {
-        let pageNav = new PageNav(".media-stream--container", ".media-stream--section");
-    }
+  // Scroll From Screen
+  if (exists(".hint-from-top")) scrollFromScreen(".screen-top");
 
-    // Scroll From Screen
-    if (exists(".hint-from-top")) scrollFromScreen(".screen-top");
+  // Parallax Scroll
+  if (exists(".screen-head")) {
+    new ParallaxScroll(".screen-head", ".screen-bg", 200);
+  }
 
-    // Parallax Scroll
-    if (exists(".screen-head")) {
-        new ParallaxScroll(".screen-head", ".screen-bg", 200);
-    }
+  // Select Custom
+  if (exists(".select-custom")) {
+    customSelect();
+    const selectIndustriesIcon = document.querySelector(
+      ".select-industries-icon img"
+    );
+    const selectIndustries = document.querySelector(".select-custom select");
+    const blockForLoad = document.getElementById("content-load");
 
-    // Select Custom
-    if (exists(".select-custom")) {
-        customSelect();
-        const selectIndustriesIcon = document.querySelector(".select-industries-icon img");
-        const selectIndustries = document.querySelector(".select-custom select");
-        const blockForLoad = document.getElementById("content-load");
+    selectIndustries.addEventListener("change", () => {
+      let index = selectIndustries.value;
+      let getFileName = `_res-content-${index}.html`;
+      selectIndustriesIcon.src = `images/ind-${index}.svg`;
 
-        selectIndustries.addEventListener("change", () => {
-            let index = selectIndustries.value;
-            let getFileName = `_res-content-${index}.html`;
-            selectIndustriesIcon.src = `images/ind-${index}.svg`;
-
-            loadContent(blockForLoad, getFileName);
-            changeFileList(index);
-        });
-
-
-
-        let changeFileList = (index) => {
-            if (exists(".file-list")) {
-                document.querySelector(".file-list.show").classList.remove("show");
-                document.querySelector(`#file-list-${index}`).classList.add("show");
-            }
-        }
-    }
-
-    // Load More
-    const buttonsMore = document.querySelectorAll(".btn-show-more");
-
-    buttonsMore.forEach(button => {
-        button.addEventListener("click", () => {
-            let parentBlock = findParent(button, "media-stream--section");
-            let loadPlace = parentBlock.querySelector(".media-stream--load-place .media-stream--cols");
-
-            loadContent(loadPlace, "_load-more-resources.html");
-            button.parentNode.classList.add("hide");
-        });
-    })
-
-    // Lazy Loader Images
-    const images = document.querySelectorAll(".lazy-img");
-
-    images.forEach(image => {
-        const mediaQueryImages = [{
-                media: "screen and (min-width: 768px)",
-                src: image.dataset.src
-            },
-            {
-                media: "screen and (max-width: 767px)",
-                src: image.dataset.srcmob
-            }
-        ];
-
-        new MediaLoader(image, "image", mediaQueryImages);
+      loadContent(blockForLoad, getFileName);
+      changeFileList(index);
     });
 
-    // Filter Mobile
-    filterSidebarInit();
+    let changeFileList = index => {
+      if (exists(".file-list")) {
+        document.querySelector(".file-list.show").classList.remove("show");
+        document.querySelector(`#file-list-${index}`).classList.add("show");
+      }
+    };
+  }
 
+  // Load More
+  const buttonsMore = document.querySelectorAll(".btn-show-more");
+
+  buttonsMore.forEach(button => {
+    button.addEventListener("click", () => {
+      let parentBlock = findParent(button, "media-stream--section");
+      let loadPlace = parentBlock.querySelector(
+        ".media-stream--load-place .media-stream--cols"
+      );
+
+      loadContent(loadPlace, "_load-more-resources.html");
+      button.parentNode.classList.add("hide");
+    });
+  });
+
+  // Lazy Loader Images
+  const images = document.querySelectorAll(".lazy-img");
+
+  images.forEach(image => {
+    const mediaQueryImages = [
+      {
+        media: "screen and (min-width: 768px)",
+        src: image.dataset.src
+      },
+      {
+        media: "screen and (max-width: 767px)",
+        src: image.dataset.srcmob
+      }
+    ];
+
+    new MediaLoader(image, "image", mediaQueryImages);
+  });
+
+  // Filter Mobile
+  filterSidebarInit();
 });
--- js/page-nav-anchor.js
+++ js/page-nav-anchor.js
@@ -13,92 +13,91 @@
 */
 
 export class PageNav {
+  constructor(navWrap, block, callback, sticky, stickyHeight) {
+    this.navWrap = document.querySelector(navWrap);
+    this.sticky = sticky || false;
+    this.stickyHeight = stickyHeight || null;
+    this.block = document.querySelectorAll(block);
+    this.stickyTopMargin = 0;
+    this.callback = callback;
 
-    constructor(navWrap, block, callback, sticky, stickyHeight) {
-        this.navWrap = document.querySelector(navWrap);
-        this.sticky = sticky || false;
-        this.stickyHeight = stickyHeight || null;
-        this.block = document.querySelectorAll(block);
-        this.stickyTopMargin = 0;
-        this.callback = callback;
-        
-        this.buttonClickHandler();
-        this.onScreenHandler();
-        this.scrollingHandler();
+    this.buttonClickHandler();
+    this.onScreenHandler();
+    this.scrollingHandler();
+  }
+
+  buttonClickHandler() {
+    this.navWrap.addEventListener("click", e => {
+      let target = e.target.closest("li[data-hash]");
+
+      if (target) {
+        let hash = target.getAttribute("data-hash");
+        this.scrollToBlock(hash);
+        this.changeActiveButton(target);
+      }
+    });
+  }
+
+  scrollToBlock(hash) {
+    let toBlock = document.getElementById(hash);
+    let blockOffsetTop = toBlock.getBoundingClientRect().top + window.scrollY;
+
+    document.documentElement.scrollTop = blockOffsetTop - this.stickyTopMargin;
+    document.body.scrollTop = blockOffsetTop - this.stickyTopMargin; // For IE
+  }
+
+  changeActiveButton(element) {
+    if (!element.classList.contains("active")) {
+      this.navWrap.querySelector("li.active").classList.remove("active");
+      element.classList.add("active");
     }
+  }
 
-    buttonClickHandler() {
-        this.navWrap.addEventListener("click", (e) => {
-            let target = e.target.closest("li[data-hash]");
-           
-            if (target) {
-                let hash = target.getAttribute("data-hash");
-                this.scrollToBlock(hash);
-                this.changeActiveButton(target);
-            }
-        });
-    }
+  scrollingHandler() {
+    window.addEventListener("scroll", e => {
+      this.onScreenHandler();
 
-    scrollToBlock(hash) {
-        let toBlock = document.getElementById(hash);
-        let blockOffsetTop = toBlock.getBoundingClientRect().top + window.scrollY;
+      if (this.sticky) this.stickyNav(this.stickyHeight);
+    });
+  }
 
-        document.documentElement.scrollTop = blockOffsetTop - this.stickyTopMargin;
-        document.body.scrollTop = blockOffsetTop - this.stickyTopMargin; // For IE
-    }
+  stickyNav(stickyHeight) {
+    window.pageYOffset > stickyHeight
+      ? this.navWrap.classList.add("sticky")
+      : this.navWrap.classList.remove("sticky");
+  }
 
-    changeActiveButton(element) {
-        if (!element.classList.contains("active")) {
-            this.navWrap.querySelector("li.active").classList.remove("active");
-            element.classList.add("active");
+  onScreenHandler() {
+    console.log(this.block);
+    this.block.forEach(block => {
+      let id = block.getAttribute("id");
+
+      if (this.onScreenDetect(block, 0.2)) {
+        let buttonNav = this.navWrap.querySelector(`[data-hash=${id}]`);
+        let itemsElement,
+          items = null;
+        let buttonNavName =
+          buttonNav.querySelector("span").textContent ||
+          buttonNav.querySelector("span").innerText;
+        this.changeActiveButton(buttonNav);
+
+        if (buttonNav.querySelector(".items") != null) {
+          itemsElement = buttonNav.querySelector(".items");
+          items = itemsElement.textContent || itemsElement.innerText;
         }
-    }
 
-    scrollingHandler() {
-        
-        window.addEventListener("scroll", e => {
-            this.onScreenHandler();
+        if (this.callback) this.callback(buttonNavName, items);
+      }
+    });
+  }
 
-            if (this.sticky) this.stickyNav(this.stickyHeight);
-        });
-    }
+  onScreenDetect(element) {
+    let scrollingElement =
+        document.scrollingElement || document.documentElement,
+      viewTop = scrollingElement.scrollTop,
+      _top =
+        element.getBoundingClientRect().top + viewTop - window.innerHeight / 2;
 
-    stickyNav(stickyHeight) {
-        (window.pageYOffset > stickyHeight) 
-            ? this.navWrap.classList.add("sticky")
-            : this.navWrap.classList.remove("sticky");
-    }
-
-    onScreenHandler() {
-        this.block.forEach( block => {
-            let id = block.getAttribute("id");
-                
-            if (this.onScreenDetect(block, 0.2)) {
-                let buttonNav = this.navWrap.querySelector(`[data-hash=${id}]`);
-                let itemsElement, items = null;
-                let buttonNavName = buttonNav.querySelector("span").textContent || buttonNav.querySelector("span").innerText;
-                this.changeActiveButton(buttonNav);
-
-                if (buttonNav.querySelector(".items") != null) {
-                    itemsElement = buttonNav.querySelector(".items");
-                    items = itemsElement.textContent || itemsElement.innerText;
-                }
-                
-                if (this.callback) this.callback(buttonNavName, items);
-            }
-        });
-    }
-
-    onScreenDetect(element, partial) {
-        let scrollingElement = document.scrollingElement || document.documentElement,
-            viewTop          = scrollingElement.scrollTop,
-            viewBottom       = viewTop + window.innerHeight,
-            _top             = element.getBoundingClientRect().top + viewTop - (window.innerHeight / 2),
-            _bottom          = _top + element.offsetHeight,
-            compareTop       = partial === true ? _bottom : _top,
-            compareBottom    = partial === true ? _top : _bottom;
-            
-            return (viewTop >= _top);
-            // return ((compareBottom <= viewBottom) && (compareTop >= viewTop));
-    }
+    return viewTop >= _top;
+  }
 }
