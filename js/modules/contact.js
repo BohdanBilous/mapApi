2d887807fc65b600e072f82eacc45f020ad4d639
Yura Ugryn <krabydr@gmail.com>
2019-11-19 16:08:42 +0200

    change map

--- js/modules/contact.js
+++ js/modules/contact.js
@@ -67,33 +67,33 @@ window.addEventListener("load", function () {
 
     // Select Custom
 
-    if (exists(".select-custom")) {
-        customSelect();
-        const selectCoutry = document.querySelector(".select-custom select");
-        const changeTitle = document.querySelector(".contact-ttl-row h2 .icon");
-        selectCoutry.addEventListener("change", () => {
-            const index = selectCoutry.value;
-            setTimeout(() => {
-                const parentEl = selectCoutry.parentElement;
-                const value = parentEl.querySelector('.select-selected').innerHTML;
-                const newElement = createCoutry(index, value);
-                changeTitle.innerHTML = newElement;
-            }, 1)
-        });
+    // if (exists(".select-custom")) {
+    //     customSelect();
+    //     const selectCoutry = document.querySelector(".select-custom select");
+    //     const changeTitle = document.querySelector(".contact-ttl-row h2 .icon");
+    //     selectCoutry.addEventListener("change", () => {
+    //         const index = selectCoutry.value;
+    //         setTimeout(() => {
+    //             const parentEl = selectCoutry.parentElement;
+    //             const value = parentEl.querySelector('.select-selected').innerHTML;
+    //             const newElement = createCoutry(index, value);
+    //             changeTitle.innerHTML = newElement;
+    //         }, 1)
+    //     });
 
 
-        // Tabs
-        if (exists(".contact-tabs-container")) {
-            new selectSwitcher(".contact-tabs-container");
-        }
+    //     // Tabs
+    //     if (exists(".contact-tabs-container")) {
+    //         new selectSwitcher(".contact-tabs-container");
+    //     }
 
 
 
 
-        function createCoutry(image, value) {
-            return image !== "all" ? `/ <img src="./images/flag-${image}.png"/>${value}` : null;
-        };
-    }
+    //     function createCoutry(image, value) {
+    //         return image !== "all" ? `/ <img src="./images/flag-${image}.png"/>${value}` : null;
+    //     };
+    // }
 
 
     if (exists(".searchable")) {
@@ -110,7 +110,7 @@ window.addEventListener("load", function () {
             center: [36.057944835, -112.18688965],
             zoom: 3,
             dragging: true,
-            scrollWheelZoom: true
+            scrollWheelZoom: false
         });
 
         WE.tileLayer('https://api.maptiler.com/maps/positron/256/{z}/{x}/{y}.png?key=DV0Wcpnxa5xR0MwfweYz', {
