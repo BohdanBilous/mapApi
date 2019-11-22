5aeea3e50a19f54dceff95f621e783ab4c729abc
Bohdan <bohdan.bilous2@gmail.com>
2019-11-22 12:12:30 +0200

    about us video section change

--- js/video-lazy-load.js
+++ js/video-lazy-load.js
@@ -2,44 +2,50 @@
     
 */
 
-
 export class VideoLoad {
+  constructor(
+    videoWrapSelector,
+    controls,
+    autoplay = false,
+    muted,
+    loop = true,
+    callback = null
+  ) {
+    this.videoWrap = document.querySelector(videoWrapSelector);
+    this.videoSrc = this.videoWrap.dataset.src;
+    this.videoPoster = this.videoWrap.dataset.poster;
+    this.controls = controls || false;
+    this.autoplay = autoplay;
+    this.muted = muted || true;
+    this.loop = loop;
+    this.videoElement = null;
+    this.modalVideo = document.querySelector(".modal-video") || null;
+    this.callback = callback;
+    this.videoInit();
+    this.addPoster();
+    this.videoSetAttr();
+    console.log(this.videoWrap);
+  }
 
-    constructor(videoWrapSelector, controls, autoplay = false, muted, loop = true, callback = null) {
-        this.videoWrap = document.querySelector(videoWrapSelector);
-        this.videoSrc = this.videoWrap.dataset.src;
-        this.videoPoster = this.videoWrap.dataset.poster;
-        this.controls = controls || false;
-        this.autoplay = autoplay ;
-        this.muted = muted || true;
-        this.loop = loop;
-        this.videoElement = null;
-        this.modalVideo = document.querySelector(".modal-video") || null;
-        this.callback =callback;
-        this.videoInit();
-        this.addPoster();
-        this.videoSetAttr();
+  videoInit() {
+    this.videoElement = document.createElement("video");
+
+    this.videoElement.setAttribute("src", this.videoSrc);
+    this.videoWrap.appendChild(this.videoElement);
+    this.videoElement.load();
+    if (this.callback) this.callback();
+  }
+
+  videoSetAttr() {
+    if (this.controls) this.videoElement.controls = true;
+    if (this.autoplay) this.videoElement.autoplay = true;
+    if (this.muted) this.videoElement.muted = true;
+    if (this.loop) this.videoElement.loop = true;
+  }
+
+  addPoster() {
+    if (this.videoPoster) {
+      this.videoElement.setAttribute("poster", this.videoPoster);
     }
-
-    videoInit() {
-        this.videoElement = document.createElement("video");
-
-        this.videoElement.setAttribute("src", this.videoSrc);
-        this.videoWrap.appendChild(this.videoElement);
-        this.videoElement.load();
-        this.callback(this);
-    }
-
-    videoSetAttr() {
-        if (this.controls) this.videoElement.controls = true;
-        if (this.autoplay) this.videoElement.autoplay = true;
-        if (this.muted) this.videoElement.muted = true;
-        if (this.loop) this.videoElement.loop = true;
-    }
-
-    addPoster() {
-        if (this.videoPoster) {
-            this.videoElement.setAttribute("poster", this.videoPoster);
-        }
-    }
+  }
 }
