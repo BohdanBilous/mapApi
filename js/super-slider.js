db6918fbc1f02185c37aecfbec66478d9b14a673
Yura Ugryn <krabydr@gmail.com>
2019-11-25 11:48:26 +0200

    Fix click

--- js/super-slider.js
+++ js/super-slider.js
@@ -244,174 +244,177 @@ export class SuperSlider {
       false;
 
     if (Math.abs(this.moveX - this.startX) === 0) return;
-
     if (fader) {
-      (!stayAtCurrent) && this.moveX > this.startX ?
-        this.faderMovePrev() :
-        this.faderMoveNext();
+      if (!stayAtCurrent) {
+        this.moveX > this.startX ?
+          this.faderMovePrev() :
+          this.faderMoveNext();
+      }
     } else {
-      (!stayAtCurrent) && this.moveX > this.startX ?
-          this.carouselMovePrev() :
-          this.carouselMoveNext();
-    }
-    delete this.startX;
-    delete this.startY;
-    delete this.moveX;
-    delete this.moveY;
-
-    this.sliderWrap.classList.remove("move");
-    this.sliderWrap.removeEventListener("mousemove", this.touchMove);
-    this.sliderWrap.removeEventListener("touchmove", this.touchMove);
-    this.body.removeEventListener("mouseup", this.touchEnd);
-    this.body.removeEventListener("touchend", this.touchEnd);
-  }
-
-  setInactiveButtons() {
-    if (this.buttonPrev.classList.contains("inactive"))
-      this.buttonPrev.classList.remove("inactive");
-    if (this.buttonNext.classList.contains("inactive"))
-      this.buttonNext.classList.remove("inactive");
-
-    if (this.slideIndex >= this.slidesItems - this.getItemsInViewport()) {
-      this.buttonNext.classList.add("inactive");
-    }
-
-    if (this.slideIndex == 0) {
-      this.buttonPrev.classList.add("inactive");
-    }
-  }
-
-  getCarouselWidth() {
-    return this.sliderWrap.clientWidth;
-  }
-
-  getItemsInViewport() {
-    // return visible items on viewport
-    return (this.sliderWrap.clientWidth / this.slide.clientWidth).toFixed();
-  }
-
-  getMargin() {
-    // get CSS margin-right of slide (need import 'getStyle' function)
-    let marginRight = getStyle(this.slide, "margin-right");
-    return parseInt(marginRight);
-  }
-
-  setVisibleSlides() {
-    let lastVisibleSlideIndex =
-      this.slideIndex + (this.getItemsInViewport() - 1);
-
-    Array.prototype.forEach.call(this.slides, (slide, index) => {
-      slide.classList.remove("visible");
-
-      if (index >= this.slideIndex && index <= lastVisibleSlideIndex)
-        slide.classList.add("visible");
-    });
-  }
-
-  // FADER PART
-  faderInit() {
-    if (this.fader) {
-      this.timerInterval, (this.timerSetTimeout = null);
-      this.slidesContainer = this.sliderWrap.querySelector(".slides");
-      this.slides = this.slidesContainer.querySelectorAll(".slide");
-      this.dots = this.sliderWrap.querySelectorAll(".slider-dots > li") || null;
-      this.touchHandler(this.slidesContainer);
-      if (this.timerNumber) this.setTimer(0, this.timerNumber);
-      if (this.dots) this.dotsClickHandler();
-    }
-  }
-
-  setTimer(startIndex, timerNumber) {
-    // start from 'startIndex' through 'timerNumber'
-    let slideIndex = startIndex;
-
-    this.timerInterval = setInterval(() => {
-      slideIndex = (slideIndex + 1) % this.slides.length;
-      this.changeSlide(slideIndex);
-      this.changeDots(slideIndex);
-    }, timerNumber);
-  }
-
-  clearTimer(slideIndex) {
-    // clear interval and set new timer
-    clearInterval(this.timerInterval);
-    clearTimeout(this.timerSetTimeout);
-    this.setTimer(slideIndex, this.timerNumber);
-  }
-
-  changeSlide(slideIndex) {
-    this.slideIndex = slideIndex;
-
-    if (slideIndex != undefined) {
-      this.sliderWrap.querySelector(".slide.active").classList.remove("active");
-      this.slides[slideIndex].classList.add("active");
-    }
-    if (this.pages) this.pages.querySelector('.slide-current').innerHTML = `0${slideIndex+1}`;
-    if (this.sliderCallBack) this.sliderCallBack(this);
-    if (this.dots) this.changeDots(slideIndex);
-  }
-
-  changeDots(slideIndex) {
-    if (slideIndex != undefined && this.dots.length) {
-      this.sliderWrap
-        .querySelector(".slider-dots .active")
-        .classList.remove("active");
-      this.dots[slideIndex].classList.add("active");
-    }
-  }
-
-  dotsClickHandler() {
-    Array.prototype.forEach.call(this.dots, dot => {
-      dot.addEventListener("click", () => {
-        if (!dot.classList.contains("active")) {
-          this.slideIndex = this.getIndex(dot);
-
-          this.changeSlide(this.slideIndex);
-          if (this.timerNumber) this.clearTimer(this.slideIndex);
+      if (!stayAtCurrent) {
+          this.moveX > this.startX ?
+            this.carouselMovePrev() :
+            this.carouselMoveNext();
         }
+      }
+      delete this.startX;
+      delete this.startY;
+      delete this.moveX;
+      delete this.moveY;
+
+      this.sliderWrap.classList.remove("move");
+      this.sliderWrap.removeEventListener("mousemove", this.touchMove);
+      this.sliderWrap.removeEventListener("touchmove", this.touchMove);
+      this.body.removeEventListener("mouseup", this.touchEnd);
+      this.body.removeEventListener("touchend", this.touchEnd);
+    }
+
+    setInactiveButtons() {
+      if (this.buttonPrev.classList.contains("inactive"))
+        this.buttonPrev.classList.remove("inactive");
+      if (this.buttonNext.classList.contains("inactive"))
+        this.buttonNext.classList.remove("inactive");
+
+      if (this.slideIndex >= this.slidesItems - this.getItemsInViewport()) {
+        this.buttonNext.classList.add("inactive");
+      }
+
+      if (this.slideIndex == 0) {
+        this.buttonPrev.classList.add("inactive");
+      }
+    }
+
+    getCarouselWidth() {
+      return this.sliderWrap.clientWidth;
+    }
+
+    getItemsInViewport() {
+      // return visible items on viewport
+      return (this.sliderWrap.clientWidth / this.slide.clientWidth).toFixed();
+    }
+
+    getMargin() {
+      // get CSS margin-right of slide (need import 'getStyle' function)
+      let marginRight = getStyle(this.slide, "margin-right");
+      return parseInt(marginRight);
+    }
+
+    setVisibleSlides() {
+      let lastVisibleSlideIndex =
+        this.slideIndex + (this.getItemsInViewport() - 1);
+
+      Array.prototype.forEach.call(this.slides, (slide, index) => {
+        slide.classList.remove("visible");
+
+        if (index >= this.slideIndex && index <= lastVisibleSlideIndex)
+          slide.classList.add("visible");
       });
-    });
+    }
+
+    // FADER PART
+    faderInit() {
+      if (this.fader) {
+        this.timerInterval, (this.timerSetTimeout = null);
+        this.slidesContainer = this.sliderWrap.querySelector(".slides");
+        this.slides = this.slidesContainer.querySelectorAll(".slide");
+        this.dots = this.sliderWrap.querySelectorAll(".slider-dots > li") || null;
+        this.touchHandler(this.slidesContainer);
+        if (this.timerNumber) this.setTimer(0, this.timerNumber);
+        if (this.dots) this.dotsClickHandler();
+      }
+    }
+
+    setTimer(startIndex, timerNumber) {
+      // start from 'startIndex' through 'timerNumber'
+      let slideIndex = startIndex;
+
+      this.timerInterval = setInterval(() => {
+        slideIndex = (slideIndex + 1) % this.slides.length;
+        this.changeSlide(slideIndex);
+        this.changeDots(slideIndex);
+      }, timerNumber);
+    }
+
+    clearTimer(slideIndex) {
+      // clear interval and set new timer
+      clearInterval(this.timerInterval);
+      clearTimeout(this.timerSetTimeout);
+      this.setTimer(slideIndex, this.timerNumber);
+    }
+
+    changeSlide(slideIndex) {
+      this.slideIndex = slideIndex;
+
+      if (slideIndex != undefined) {
+        this.sliderWrap.querySelector(".slide.active").classList.remove("active");
+        this.slides[slideIndex].classList.add("active");
+      }
+      if (this.pages) this.pages.querySelector('.slide-current').innerHTML = `0${slideIndex+1}`;
+      if (this.sliderCallBack) this.sliderCallBack(this);
+      if (this.dots) this.changeDots(slideIndex);
+    }
+
+    changeDots(slideIndex) {
+      if (slideIndex != undefined && this.dots.length) {
+        this.sliderWrap
+          .querySelector(".slider-dots .active")
+          .classList.remove("active");
+        this.dots[slideIndex].classList.add("active");
+      }
+    }
+
+    dotsClickHandler() {
+      Array.prototype.forEach.call(this.dots, dot => {
+        dot.addEventListener("click", () => {
+          if (!dot.classList.contains("active")) {
+            this.slideIndex = this.getIndex(dot);
+
+            this.changeSlide(this.slideIndex);
+            if (this.timerNumber) this.clearTimer(this.slideIndex);
+          }
+        });
+      });
+    }
+
+    buttonsClickHandler() {
+      this.buttonNext.addEventListener(
+        "click",
+        function (e) {
+          if (!this.buttonNext.classList.contains("inactive")) {
+            if (this.fader) this.faderMoveNext();
+            if (this.carousel) this.carouselMoveNext();
+          }
+        }.bind(this)
+      );
+
+      this.buttonPrev.addEventListener(
+        "click",
+        function (e) {
+          if (!this.buttonPrev.classList.contains("inactive")) {
+            if (this.fader) this.faderMovePrev();
+            if (this.carousel) this.carouselMovePrev();
+          }
+        }.bind(this)
+      );
+    }
+
+    faderMoveNext() {
+      this.slideIndex = (this.slideIndex + 1) % this.slides.length;
+      this.changeSlide(this.slideIndex);
+    }
+
+    faderMovePrev() {
+      this.slideIndex == 0 ?
+        (this.slideIndex = this.slides.length) :
+        this.slideIndex - 1;
+      this.changeSlide(this.slideIndex - 1);
+    }
+
+    getIndex(element) {
+      let parent = element.parentNode;
+      let elementsList = parent.children;
+      let index = Array.prototype.indexOf.call(elementsList, element);
+
+      return index;
+    }
   }
-
-  buttonsClickHandler() {
-    this.buttonNext.addEventListener(
-      "click",
-      function (e) {
-        if (!this.buttonNext.classList.contains("inactive")) {
-          if (this.fader) this.faderMoveNext();
-          if (this.carousel) this.carouselMoveNext();
-        }
-      }.bind(this)
-    );
-
-    this.buttonPrev.addEventListener(
-      "click",
-      function (e) {
-        if (!this.buttonPrev.classList.contains("inactive")) {
-          if (this.fader) this.faderMovePrev();
-          if (this.carousel) this.carouselMovePrev();
-        }
-      }.bind(this)
-    );
-  }
-
-  faderMoveNext() {
-    this.slideIndex = (this.slideIndex + 1) % this.slides.length;
-    this.changeSlide(this.slideIndex);
-  }
-
-  faderMovePrev() {
-    this.slideIndex == 0 ?
-      (this.slideIndex = this.slides.length) :
-      this.slideIndex - 1;
-    this.changeSlide(this.slideIndex - 1);
-  }
-
-  getIndex(element) {
-    let parent = element.parentNode;
-    let elementsList = parent.children;
-    let index = Array.prototype.indexOf.call(elementsList, element);
-
-    return index;
-  }
-}
