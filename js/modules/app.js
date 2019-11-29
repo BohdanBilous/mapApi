5a46ad80b270778429b8feb339df7e6872aebc32
Yura Ugryn <krabydr@gmail.com>
2019-11-29 14:54:07 +0200

    tablet

--- js/modules/app.js
+++ js/modules/app.js
@@ -3,7 +3,7 @@
 /* MODULE IMPORTS */
 // import "../../vendor/modernizr-custom";
 // import {
-//   html,
+  html,
 //   // desktop,
 //   // exists,
 //   // animateSwitch,
@@ -11,7 +11,7 @@
 //   // ssHashLink
 // } from "../generic-helpers";
 
-document.querySelector(".screen-main .ttl-1").innerHTML = "html";
+document.querySelector(".screen-main .ttl-1").innerHTML = html;
 console.log("== Begin executing app.js ==");
 
 document.documentElement.className = document.documentElement.className.replace(
