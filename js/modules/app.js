2c4a38bda8e25fa3dbf7d38b5dd361d8c2e162d8
Bohdan <bohdan.bilous2@gmail.com>
2019-11-29 14:00:40 +0200

    iOS debuging: remove app

--- js/modules/app.js
+++ js/modules/app.js
@@ -1,15 +1,15 @@
 /*jshint esversion: 6 */
 
 /* MODULE IMPORTS */
-import "../../vendor/modernizr-custom";
-import {
-  html,
-  desktop,
-  exists,
-  animateSwitch,
-  buttonArrowAnimate,
-  ssHashLink
-} from "../generic-helpers";
+// import "../../vendor/modernizr-custom";
+// import {
+//   html,
+//   desktop,
+//   exists,
+//   animateSwitch,
+//   buttonArrowAnimate,
+//   ssHashLink
+// } from "../generic-helpers";
 
 console.log("== Begin executing app.js ==");
 
@@ -18,158 +18,158 @@ document.documentElement.className = document.documentElement.className.replace(
   ""
 );
 
-window.addEventListener("load", function() {
-  const consoleRow = document.createElement("div");
-  consoleRow.classList.add("console-row");
-  document.body.appendChild(consoleRow);
+// window.addEventListener("load", function() {
+//   const consoleRow = document.createElement("div");
+//   consoleRow.classList.add("console-row");
+//   document.body.appendChild(consoleRow);
 
-  /* ------------------
-      Header
-    --------------------- */
+//   /* ------------------
+//       Header
+//     --------------------- */
 
-  // hide-activate header
-  // let lastScrollTop = 0;
+//   // hide-activate header
+//   // let lastScrollTop = 0;
 
-  // window.addEventListener("scroll", function(){
-  //   var st = window.pageYOffset || document.documentElement.scrollTop;
-  //   if (st > lastScrollTop){
-  //       document.querySelector(".header").classList.add("hide-header");
-  //   } else {
-  //       document.querySelector(".header").classList.remove("hide-header");
-  //   }
-  //   lastScrollTop = st;
-  // }, false);
+//   // window.addEventListener("scroll", function(){
+//   //   var st = window.pageYOffset || document.documentElement.scrollTop;
+//   //   if (st > lastScrollTop){
+//   //       document.querySelector(".header").classList.add("hide-header");
+//   //   } else {
+//   //       document.querySelector(".header").classList.remove("hide-header");
+//   //   }
+//   //   lastScrollTop = st;
+//   // }, false);
 
-  // Drop Down
-  if (exists(".drop-down-element")) {
-    document.querySelectorAll(".drop-down-element").forEach(dropDownElement => {
-      dropDownElement
-        .querySelector(".drop-down-sel")
-        .addEventListener("click", e => {
-          e.stopPropagation();
-          dropDownElement.classList.contains("open")
-            ? dropDownElement.classList.remove("open")
-            : dropDownElement.classList.add("open");
-          if (dropDownElement.classList.contains("open")) {
-            window.addEventListener("click", () => {
-              dropDownElement.classList.remove("open");
-            });
-          }
-        });
-    });
-  }
+//   // Drop Down
+//   if (exists(".drop-down-element")) {
+//     document.querySelectorAll(".drop-down-element").forEach(dropDownElement => {
+//       dropDownElement
+//         .querySelector(".drop-down-sel")
+//         .addEventListener("click", e => {
+//           e.stopPropagation();
+//           dropDownElement.classList.contains("open")
+//             ? dropDownElement.classList.remove("open")
+//             : dropDownElement.classList.add("open");
+//           if (dropDownElement.classList.contains("open")) {
+//             window.addEventListener("click", () => {
+//               dropDownElement.classList.remove("open");
+//             });
+//           }
+//         });
+//     });
+//   }
 
-  // Menu
-  const menuSwitcher = document.querySelector(".menu-switcher");
+//   // Menu
+//   const menuSwitcher = document.querySelector(".menu-switcher");
 
-  menuSwitcher.addEventListener("click", () => {
-    if (html.classList.contains("menu-open")) {
-      html.classList.remove("menu-open");
-      html.classList.add("menu-close");
-      animateSwitch(".menu .fade-in", "off");
-      animateSwitch(".menu .move-from-right", "off");
-    } else {
-      html.classList.add("menu-open");
-      html.classList.remove("menu-close");
-      animateSwitch(".menu .fade-in", "on", 1, 800);
-      animateSwitch(".menu .move-from-right", "on", 65, 250);
-    }
-  });
+//   menuSwitcher.addEventListener("click", () => {
+//     if (html.classList.contains("menu-open")) {
+//       html.classList.remove("menu-open");
+//       html.classList.add("menu-close");
+//       animateSwitch(".menu .fade-in", "off");
+//       animateSwitch(".menu .move-from-right", "off");
+//     } else {
+//       html.classList.add("menu-open");
+//       html.classList.remove("menu-close");
+//       animateSwitch(".menu .fade-in", "on", 1, 800);
+//       animateSwitch(".menu .move-from-right", "on", 65, 250);
+//     }
+//   });
 
-  /* ------------------
-        Buttons
-    --------------------- */
+//   /* ------------------
+//         Buttons
+//     --------------------- */
 
-  // Button Animate
-  if (desktop) buttonArrowAnimate();
+//   // Button Animate
+//   if (desktop) buttonArrowAnimate();
 
-  // Button Change Text
-  if (exists(".btn-change-text")) {
-    const btnChangeText = document.querySelectorAll(".btn-change-text");
-    let btnText, btnDataText, btnMedia;
+//   // Button Change Text
+//   if (exists(".btn-change-text")) {
+//     const btnChangeText = document.querySelectorAll(".btn-change-text");
+//     let btnText, btnDataText, btnMedia;
 
-    btnChangeText.forEach(btn => {
-      btnDataText = btn.dataset.text;
-      btnText = btn.querySelector("span").innerHTML;
-      btnMedia = btn.dataset.media;
+//     btnChangeText.forEach(btn => {
+//       btnDataText = btn.dataset.text;
+//       btnText = btn.querySelector("span").innerHTML;
+//       btnMedia = btn.dataset.media;
 
-      if (window.matchMedia(btnMedia).matches) {
-        btn.querySelector("span").innerHTML = btnDataText;
-        btn.setAttribute("data-text", btnText);
-      }
-    });
-  }
+//       if (window.matchMedia(btnMedia).matches) {
+//         btn.querySelector("span").innerHTML = btnDataText;
+//         btn.setAttribute("data-text", btnText);
+//       }
+//     });
+//   }
 
-  // Links Locking
-  // const hrefArray = ["case-open.html", "event-open.html"];
-  // document.querySelectorAll("a").forEach(link => {
-  //   if (hrefArray.includes(link.getAttribute("href"))) {
-  //     link.addEventListener("click", e => e.preventDefault());
-  //   }
-  // });
+//   // Links Locking
+//   // const hrefArray = ["case-open.html", "event-open.html"];
+//   // document.querySelectorAll("a").forEach(link => {
+//   //   if (hrefArray.includes(link.getAttribute("href"))) {
+//   //     link.addEventListener("click", e => e.preventDefault());
+//   //   }
+//   // });
 
-  // Links Hash Redirect
-  ssHashLink(document.querySelectorAll(".btn-hash"));
+//   // Links Hash Redirect
+//   ssHashLink(document.querySelectorAll(".btn-hash"));
 
-  //SEARCH
-  // let inputSearch = document.getElementById("serach-input-form");
-  // let searchForm = document.querySelector(".search-form");
-  // let clearInput = inputSearch.parentNode.querySelector(".clear-input");
-  // inputSearch.focus();
-  // clearInput.style.left = `${inputSearch.value.length * 40 + 40}px`;
-  // inputSearch.selectionStart = inputSearch.selectionEnd =
-  //   inputSearch.value.length;
-  // inputSearch.value.length === 0 && clearInput.classList.add("hide");
-  // clearInput.addEventListener("click", () => {
-  //   inputSearch.value = "";
-  //   inputSearch.focus();
-  //   clearInput.classList.add("hide");
-  // });
-  // inputSearch.addEventListener("input", () => {
-  //   let hiddenElement = document.querySelector(".hidden-width");
-  //   inputSearch.value.length === 0
-  //     ? clearInput.classList.add("hide")
-  //     : clearInput.classList.remove("hide");
-  //   let valyeInput = inputSearch.value.replace(/\s/g, "|");
-  //   hiddenElement.innerHTML = valyeInput;
-  //   let hiddenElementWidth = hiddenElement.offsetWidth;
-  //   clearInput.style.left = `${hiddenElementWidth + 15}px`;
-  // });
-  // searchForm.addEventListener("submit", e => {
-  //   const data = e.target.querySelector("input").value;
-  //   document
-  //     .querySelector(".search-socket")
-  //     .classList.remove("search-find", "search-not-find");
-  //   if (data.length > 0) {
-  //     document.querySelector(".search-socket").classList.add("search-find");
-  //   } else {
-  //     document.querySelector(".search-socket").classList.add("search-not-find");
-  //   }
-  // });
-  //search-find
-  // Seach Modal
+//   //SEARCH
+//   // let inputSearch = document.getElementById("serach-input-form");
+//   // let searchForm = document.querySelector(".search-form");
+//   // let clearInput = inputSearch.parentNode.querySelector(".clear-input");
+//   // inputSearch.focus();
+//   // clearInput.style.left = `${inputSearch.value.length * 40 + 40}px`;
+//   // inputSearch.selectionStart = inputSearch.selectionEnd =
+//   //   inputSearch.value.length;
+//   // inputSearch.value.length === 0 && clearInput.classList.add("hide");
+//   // clearInput.addEventListener("click", () => {
+//   //   inputSearch.value = "";
+//   //   inputSearch.focus();
+//   //   clearInput.classList.add("hide");
+//   // });
+//   // inputSearch.addEventListener("input", () => {
+//   //   let hiddenElement = document.querySelector(".hidden-width");
+//   //   inputSearch.value.length === 0
+//   //     ? clearInput.classList.add("hide")
+//   //     : clearInput.classList.remove("hide");
+//   //   let valyeInput = inputSearch.value.replace(/\s/g, "|");
+//   //   hiddenElement.innerHTML = valyeInput;
+//   //   let hiddenElementWidth = hiddenElement.offsetWidth;
+//   //   clearInput.style.left = `${hiddenElementWidth + 15}px`;
+//   // });
+//   // searchForm.addEventListener("submit", e => {
+//   //   const data = e.target.querySelector("input").value;
+//   //   document
+//   //     .querySelector(".search-socket")
+//   //     .classList.remove("search-find", "search-not-find");
+//   //   if (data.length > 0) {
+//   //     document.querySelector(".search-socket").classList.add("search-find");
+//   //   } else {
+//   //     document.querySelector(".search-socket").classList.add("search-not-find");
+//   //   }
+//   // });
+//   //search-find
+//   // Seach Modal
 
-  // const searchOpen = document.querySelector(".search-switcher");
-  // const searchClose = document.querySelector(".search-close");
-  // const searchModal = document.querySelector(".search-modal");
-  // searchClose.addEventListener("click", () => {
-  //   html.classList.remove("search-open");
-  //   searchModal.classList.remove("open");
-  //   animateSwitch(".search .fade-in", "off");
-  //   animateSwitch(".search .move-from-right", "off");
-  // });
-  // searchOpen.addEventListener("click", () => {
-  //   html.classList.add("search-open");
-  //   searchModal.classList.add("open");
-  //   animateSwitch(".search .fade-in", "on", 1, 800);
-  //   animateSwitch(".search .move-from-right", "on", 65, 250);
-  // });
-});
+//   // const searchOpen = document.querySelector(".search-switcher");
+//   // const searchClose = document.querySelector(".search-close");
+//   // const searchModal = document.querySelector(".search-modal");
+//   // searchClose.addEventListener("click", () => {
+//   //   html.classList.remove("search-open");
+//   //   searchModal.classList.remove("open");
+//   //   animateSwitch(".search .fade-in", "off");
+//   //   animateSwitch(".search .move-from-right", "off");
+//   // });
+//   // searchOpen.addEventListener("click", () => {
+//   //   html.classList.add("search-open");
+//   //   searchModal.classList.add("open");
+//   //   animateSwitch(".search .fade-in", "on", 1, 800);
+//   //   animateSwitch(".search .move-from-right", "on", 65, 250);
+//   // });
+// });
 
-Object.defineProperty(HTMLMediaElement.prototype, "playing", {
-  get: function() {
-    return this.currentTime > 0 && !this.paused;
-  }
-});
+// Object.defineProperty(HTMLMediaElement.prototype, "playing", {
+//   get: function() {
+//     return this.currentTime > 0 && !this.paused;
+//   }
+// });
 
-console.log("== end executing app.js ==");
+// console.log("== end executing app.js ==");
