c54380bc1263055ff228c4dda94a186589dfcba6
Yura Ugryn <krabydr@gmail.com>
2019-12-06 17:19:58 +0200

    fix ie

--- js/modules/app.js
+++ js/modules/app.js
@@ -23,7 +23,6 @@ document.documentElement.className = document.documentElement.className.replace(
 );
 
 // For IE 11 ONLoad not work corectly
-document.querySelector('.logo').innerHTML= detectIE();
 if (detectIE() === "11") {
   (function () {
     if (window.localStorage) {
