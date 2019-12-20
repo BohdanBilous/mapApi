a6cd5874e8052f5cda5243e7e7a17fe52d209811
Bohdan <bohdan.bilous2@gmail.com>
2019-12-20 17:38:48 +0200

    about us: opt

--- js/modules/about-us.js
+++ js/modules/about-us.js
@@ -1,95 +1,91 @@
 /*jshint esversion: 6 */
 
 /* MODULE IMPORTS */
-import {
-    html,
-    isInView,
-    loadContent
-} from '../generic-helpers';
-import {
-    SuperSlider
-} from '../super-slider';
-import {
-    VideoLoad
-} from "../video-lazy-load";
+import { html, isInView, loadContent } from "../generic-helpers";
+import { SuperSlider } from "../super-slider";
+import { MediaLoader } from "../media-loader";
 
+window.addEventListener("load", function() {
+  // let pageNav = new PageNav(".media-stream--container", ".terms-ans");
 
+  //   new MediaLoader();
 
-window.addEventListener("load", function () {
-    // let pageNav = new PageNav(".media-stream--container", ".terms-ans");
-
-    new SuperSlider(`.about-us-fader`, "fader");
-    new SuperSlider(`.years-slider-socket`, "fader", null, function (props) {
-        const {
-            slideIndex,
-            sliderWrap,
-            dots
-        } = props;
-        const line = sliderWrap.querySelector('.line');
-        line.style.left = `${(slideIndex) * 25}%`;
-        dots.forEach(item => item.classList.remove('last-active'));
-        if (dots[slideIndex - 1]) {
-            dots[slideIndex - 1].classList.add('last-active');
-        }
-    });
-    new SuperSlider(`.fader-recycling`, "fader");
-    new SuperSlider(`.fader-saving`, "fader");
-    new SuperSlider(`.team-carosuel`, "carousel");
-
-    // new VideoLoad(".recycling-video.bg-img", false, false, true, true, true, function (props) {
-    const videoElement = document.querySelector('.recycling-video video');
-    const videoElementPlay = document.querySelector('.recycling-video .play-button-wrap')
-
-    const playVideo = (e) => {
-        isInView(videoElement) ? videoElement.play() : videoElement.pause();
+  new SuperSlider(`.about-us-fader`, "fader");
+  new SuperSlider(`.years-slider-socket`, "fader", null, function(props) {
+    const { slideIndex, sliderWrap, dots } = props;
+    const line = sliderWrap.querySelector(".line");
+    line.style.left = `${slideIndex * 25}%`;
+    dots.forEach(item => item.classList.remove("last-active"));
+    if (dots[slideIndex - 1]) {
+      dots[slideIndex - 1].classList.add("last-active");
     }
-    videoElement.pause();
-    if (window.innerWidth <= 768) {
-        window.addEventListener('touchmove', (e) => playVideo(e));
-    } else {
-        window.addEventListener('scroll', (e) => playVideo(e));
-    }
-    videoElementPlay.addEventListener('click',function(){
-        this.classList.add('active')
-        videoElement.play()
+  });
+  new SuperSlider(`.fader-recycling`, "fader");
+  new SuperSlider(`.fader-saving`, "fader");
+  new SuperSlider(`.team-carosuel`, "carousel");
+
+  // new VideoLoad(".recycling-video.bg-img", false, false, true, true, true, function (props) {
+  const videoElement = document.querySelector(".recycling-video video");
+  const videoElementPlay = document.querySelector(
+    ".recycling-video .play-button-wrap"
+  );
+
+  const playVideo = e => {
+    isInView(videoElement) ? videoElement.play() : videoElement.pause();
+  };
+  videoElement.pause();
+  if (window.innerWidth <= 768) {
+    window.addEventListener("touchmove", e => playVideo(e));
+  } else {
+    window.addEventListener("scroll", e => playVideo(e));
+  }
+  videoElementPlay.addEventListener("click", function() {
+    this.classList.add("active");
+    videoElement.play();
+  });
+  videoElementPlay.click();
+
+  const teamMembers = document.querySelectorAll(".team-item");
+  const popupClose = document.querySelectorAll(".popup .btn-close");
+  const popupBg = document.querySelectorAll(".popup .popup-bg");
+  const popupLoadBlock = document.querySelector(".popup .team-member-load");
+  let teamMemberId;
+
+  let loadPopupInfo = () => {
+    loadContent(popupLoadBlock, `./team-info/_load-team-info-member-1.html`);
+    // loadContent(popupLoadBlock, `_load-team-info-${teamMemberId}.html`);
+  };
+
+  teamMembers.forEach(teamMember => {
+    teamMember.addEventListener("click", () => {
+      teamMemberId = teamMember.getAttribute("id");
+      html.classList.add("popup-open", "popup-open-about");
+      html.classList.remove("popup-close");
+      loadPopupInfo(teamMemberId);
     });
-    videoElementPlay.click();
+  });
 
-    const teamMembers = document.querySelectorAll(".team-item");
-    const popupClose = document.querySelectorAll(".popup .btn-close");
-    const popupBg = document.querySelectorAll(".popup .popup-bg");
-    const popupLoadBlock = document.querySelector(".popup .team-member-load");
-    let teamMemberId;
-
-    let loadPopupInfo = () => {
-        loadContent(popupLoadBlock, `./team-info/_load-team-info-member-1.html`);
-        // loadContent(popupLoadBlock, `_load-team-info-${teamMemberId}.html`);
-    };
-
-    teamMembers.forEach(teamMember => {
-        teamMember.addEventListener("click", () => {
-            teamMemberId = teamMember.getAttribute("id");
-            html.classList.add("popup-open", "popup-open-about");
-            html.classList.remove("popup-close");
-            loadPopupInfo(teamMemberId);
-        });
-    });
-
-    const openAboutPdf = document.querySelector('.openAboutPdf');
-    openAboutPdf.addEventListener("click", () => {
-        html.classList.add("popup-open", "popup-open-abouu-us");
-        html.classList.remove("popup-close");
-    });
-    const closePopup = () => {
-        html.classList.remove("popup-open", "popup-open-about", "popup-open-abouu-us");
-        html.classList.add("popup-close");
-
-    }
-    popupClose.forEach(item => item.addEventListener("click", () => {
-        closePopup();
-    }));
-    popupBg.forEach(item => item.addEventListener("click", () => {
-        closePopup();
-    }));
-
+  const openAboutPdf = document.querySelector(".openAboutPdf");
+  openAboutPdf.addEventListener("click", () => {
+    html.classList.add("popup-open", "popup-open-abouu-us");
+    html.classList.remove("popup-close");
+  });
+  const closePopup = () => {
+    html.classList.remove(
+      "popup-open",
+      "popup-open-about",
+      "popup-open-abouu-us"
+    );
+    html.classList.add("popup-close");
+  };
+  popupClose.forEach(item =>
+    item.addEventListener("click", () => {
+      closePopup();
+    })
+  );
+  popupBg.forEach(item =>
+    item.addEventListener("click", () => {
+      closePopup();
+    })
+  );
 });
